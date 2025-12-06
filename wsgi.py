ebf39ffd7bd9b72c3f2f40d9172a0e6db44dd797
MauricioZarpelon <mauricio.lpgnr@hotmail.com>
2025-12-06 12:59:04 -0300

    Update project path in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import sys
-path = '/home/SEU_USUARIO/Projeto-Integrador-POO-Grupo-42-Senac'
+path = '/home/Darkmave/Projeto-Integrador-POO-Grupo-42-Senac'
 if path not in sys.path:
     sys.path.append(path)
 
